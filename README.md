d02143594841d6b1b5115df5436343b7e739071b
Joao Gobeti <joao.gobeti@motora.ai>
2023-11-26 12:37:21 -0300

    js using api

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # voice_assistant
 Voice assistant to record someone's speech in any language, translate it to english text, use this text as input in a llama model and return the output as speech in the same language used in input
+# Llama Chat 🦙
+
+This is a [Next.js](https://nextjs.org/) app that demonstrates how to build a chat UI using the [Llama 2](https://replicate.com/replicate/llama70b-v2-chat) language model and Replicate's [streaming API (private beta)](https://replicate.com/docs/streaming).
+
+Here's a demo:
+
+https://github.com/replicate/llama-chat/assets/14149230/9c6aaef3-4e60-4846-a2d2-f9575e155b70
+
+
+## Usage
+
+Install dependencies:
+
+```console
+npm install
+```
+
+Add your [Replicate API token](https://replicate.com/account#token) to `.env.local`:
+
+```
+REPLICATE_API_TOKEN=<your-token-here>
+```
+
+Run the development server:
+
+```console
+npm run dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser.
+
+For detailed instructions on how to create and use this template, see [replicate.com/docs/get-started/nextjs](https://replicate.com/docs/get-started/nextjs)
+
---
+++ app/api/route.js
@@ -0,0 +1,77 @@
+import Replicate from "replicate";
+import { ReplicateStream, StreamingTextResponse } from "ai";
+
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_TOKEN,
+});
+
+if (!process.env.REPLICATE_API_TOKEN) {
+  throw new Error(
+    "The REPLICATE_API_TOKEN environment variable is not set. See README.md for instructions on how to set it."
+  );
+}
+
+export const runtime = "edge";
+
+export async function POST(req) {
+  const params = await req.json();
+
+  const response = params.audio
+    ? await runSeamlessM4T(params)
+    : await runLlama(params);
+
+  // Convert the response into a friendly text-stream
+  const stream = await ReplicateStream(response);
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
+
+async function runLlama({
+  prompt,
+  systemPrompt,
+  maxTokens,
+  temperature,
+  topP,
+  version,
+}) {
+  console.log("running llama");
+  return await replicate.predictions.create({
+    // IMPORTANT! You must enable streaming.
+    stream: true,
+    input: {
+      prompt: `${prompt}`,
+      system_prompt: systemPrompt,
+      max_new_tokens: maxTokens,
+      temperature: temperature,
+      repetition_penalty: 1,
+      top_p: topP,
+    },
+    // IMPORTANT! The model must support streaming. See https://replicate.com/docs/streaming
+    version: version,
+  });
+}
+
+async function runSeamlessM4T({
+  task_name,
+  input_audio,
+  input_text_language,
+  max_input_audio_length,
+  target_language_text_only,
+  target_language_with_speech,
+}) {
+  console.log("running seamlessm4t");
+  return await replicate.predictions.create({
+    // IMPORTANT! You must enable streaming.
+    stream: true,
+    input: {
+      task_name: "S2TT (Speech to Text translation)",
+      input_audio: "https://replicate.delivery/pbxt/JWSCV0Ai3RX6k2nkLrbQILRQN0zdcJcbFFPmfD8QDzp3xZaf/sample_input.mp3",
+      input_text_language: "None",
+      max_input_audio_length: 60,
+      target_language_text_only: "Norwegian Nynorsk",
+      target_language_with_speech: "French"
+    },
+    // IMPORTANT! The model must support streaming. See https://replicate.com/docs/streaming
+    version: version,
+  });
+}
