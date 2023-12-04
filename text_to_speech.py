8c40139571ec10ecf779ef1e80c8c7d50ab974bc
Joao Felipe Gobeti <joao.gobeti@motora.ai>
2023-12-04 17:04:05 -0300

    removing obsolete file

--- text_to_speech.py
+++
@@ -1,6 +0,0 @@
-    ## For portuguese = tgt_lang="por"
-
-    #text_inputs = processor(text = llama_output, src_lang="eng", return_tensors="pt")
-    #audio_array_from_text = model.generate(**text_inputs, tgt_lang="eng")[0].cpu().numpy().squeeze()
-    #sample_rate = model.sampling_rate
-    #scipy.io.wavfile.write("llama_output.wav", rate=sample_rate, data=audio_array_from_text)
